4f6304acc3309e4220a2af744d5f5edf854f7f49
Carlos Panganiban <cgpanganiban@up.edu.ph>
2022-02-08 18:04:29 +0800

    Add tests for args/kwargs and multiple job handling (simultaneous)

--- tests/edgenet.py
+++ tests/edgenet.py
@@ -35,7 +35,7 @@ class TestNetwork(unittest.TestCase):
         self.assertNotIn(client.session_id, self.server.sessions)
 
         client.run()
-        self.server.sleep(1)
+        self.server.sleep(0.1)
 
         # Check if session_id is in server's sessions dict
         self.assertIn(client.session_id, self.server.sessions)
@@ -52,11 +52,10 @@ class TestNetwork(unittest.TestCase):
         self.assertTrue(session.websocket.open)
 
         client.close()
-        self.server.sleep(1)
+        self.server.sleep(0.1)
 
         self.assertTrue(session.websocket.closed)
         self.assertEqual(session.status, SESSION_DISCONNECTED)
-
 
     def test_client_handshake(self):
         """
@@ -95,14 +94,14 @@ class TestNetwork(unittest.TestCase):
         client.run()
         client.register_function(function_name, function_method)
 
-        self.server.sleep(1)
+        self.server.sleep(0.1)
 
         job = self.server.send_command_external(
             client.session_id, function_name, False,
             *args, **kwargs
         )
 
-        self.server.sleep(1)
+        self.server.sleep(0.1)
 
         self.assertIn(expected_result, job.raw_results)
 
@@ -123,13 +122,13 @@ class TestNetwork(unittest.TestCase):
 
         client.register_function(function_name, poll_five_times)
 
-        self.server.sleep(1)
+        self.server.sleep(0.1)
 
         job = self.server.send_command_external(
             client.session_id, function_name, True
         )
 
-        self.server.sleep(2)
+        self.server.sleep(0.5)
 
         # Check correctness of results
         for i in range(3):
@@ -137,6 +136,104 @@ class TestNetwork(unittest.TestCase):
 
         # Check if threads have closed
         for thread in client.job_threads[job.job_id]:
+            self.assertFalse(thread.is_alive())
+
+    def test_server_client_command_polling_with_args_kwargs(self):
+        """
+        Tests asynchronous polling commands called to client (with args and kwargs)
+        """
+        client = EdgeNetClient(self.server_url)
+        client.run()
+
+        function_name = "poll_five_times"
+
+        args = [623, 123]
+        kwargs = {"c": 421, "d": 125}
+
+        expected_results = [
+            f"{i} 623 123 421 125" for i in range(3)
+        ]
+
+        @client.uses_sender
+        def poll_five_times(send_result, a, b, c=1, d=2):
+            for i in range(3):
+                time.sleep(0.1)
+                send_result(f"{i} {a} {b} {c} {d}")
+
+        client.register_function(function_name, poll_five_times)
+
+        self.server.sleep(0.1)
+
+        job = self.server.send_command_external(
+            client.session_id, function_name, True,
+            *args, **kwargs
+        )
+
+        self.server.sleep(0.5)
+
+        # Check correctness of results
+        for result in expected_results:
+            self.assertIn(result, job.raw_results)
+
+        # Check if threads have closed
+        for thread in client.job_threads[job.job_id]:
+            self.assertFalse(thread.is_alive())
+
+    def test_server_client_command_polling_with_multiple_clients(self):
+        """
+        Tests asynchronous polling commands called to client (with args and kwargs)
+        """
+        client_1 = EdgeNetClient(self.server_url)
+        client_1.run()
+        client_2 = EdgeNetClient(self.server_url)
+        client_2.run()
+
+        function_name = "poll_five_times"
+
+        args_1 = [623, 123]
+        kwargs_1 = {"c": 421, "d": 125}
+        args_2 = [142437, 48126]
+        kwargs_2 = {"c": 9351, "d": 2451}
+
+        expected_results_1 = [
+            f"{i} 623 123 421 125" for i in range(3)
+        ]
+        expected_results_2 = [
+            f"{i} 142437 48126 9351 2451" for i in range(3)
+        ]
+
+        def poll_five_times(send_result, a, b, c=1, d=2):
+            for i in range(3):
+                time.sleep(0.1)
+                send_result(f"{i} {a} {b} {c} {d}")
+
+        client_1.register_function(function_name, client_1.uses_sender(poll_five_times))
+        client_2.register_function(function_name, client_2.uses_sender(poll_five_times))
+
+        self.server.sleep(0.1)
+
+        job_1 = self.server.send_command_external(
+            client_1.session_id, function_name, True,
+            *args_1, **kwargs_1
+        )
+
+        job_2 = self.server.send_command_external(
+            client_2.session_id, function_name, True,
+            *args_2, **kwargs_2
+        )
+
+        self.server.sleep(0.5)
+
+        # Check correctness of results
+        for result in expected_results_1:
+            self.assertIn(result, job_1.raw_results)
+        for result in expected_results_2:
+            self.assertIn(result, job_2.raw_results)
+
+        # Check if threads have closed
+        for thread in client_1.job_threads[job_1.job_id]:
+            self.assertFalse(thread.is_alive())
+        for thread in client_2.job_threads[job_2.job_id]:
             self.assertFalse(thread.is_alive())
 
 
