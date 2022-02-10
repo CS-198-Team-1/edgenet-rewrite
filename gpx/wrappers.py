37229eb2e2d9b6d60446316125eac90700681ccc
Carlos Panganiban <cgpanganiban@up.edu.ph>
2022-02-10 15:44:13 +0800

    Add entry fetching for GPX module

--- gpx/wrappers.py
+++ gpx/wrappers.py
@@ -14,6 +14,15 @@ class GPXCollection:
     def __repr__(self): 
         return f"<GPXCollection start:{self.start_time}, entries:{len(self.entries)}>"
 
+    def get_latest_entry(self, dttm=None):
+        if dttm is None: return self.entries[-1] # Return latest by default
+        if dttm < self.entries[0].dttm: # Throw an error if dttm is earlier than first
+            raise GPXException("There is no matching entry since the provided datetime.")
+        
+        entries_before_dttm = [e for e in self.entries if e.dttm <= dttm]
+        
+        return entries_before_dttm[-1] # Return the latest of those entries
+
 
 class GPXEntry:
     """
@@ -32,3 +41,8 @@ class GPXEntry:
 
     @property
     def lng(self): return self.latlng[1]
+
+
+class GPXException(Exception):
+    pass
+        
--- tests/gpx.py
+++ tests/gpx.py
@@ -4,7 +4,7 @@ from freezegun import freeze_time
 from unittest.mock import patch
 from dateutil import parser as dttm_parser
 from gpx import parser
-from gpx.wrappers import GPXCollection, GPXEntry
+from gpx.wrappers import GPXCollection, GPXEntry, GPXException
 
 
 class TestGPX(unittest.TestCase):
@@ -80,3 +80,60 @@ class TestGPX(unittest.TestCase):
         for i, dttm in synced_dttms.items():
             entry = gpx_collection.entries[i]
             self.assertEqual(entry.dttm, dttm)
+
+        # Assert if latlngs are still correct:
+        for i, latlng in self.latlngs.items():
+            entry = gpx_collection.entries[i]
+            self.assertEqual(entry.lat, latlng[0])
+            self.assertEqual(entry.lng, latlng[1])
+
+    @freeze_time("2022-02-10T00:00:00Z")
+    def test_get_latest_entry(self):
+        gpx_collection = parser.parse_gpx_and_sync_now(
+            self.gpx_file_path
+        )
+
+        # We should get the final entry on the GPX by default
+        entry = gpx_collection.get_latest_entry()
+        expected_time = dttm_parser.parse("2022-02-10T00:00:09Z").replace(tzinfo=None)
+        
+        self.assertEquals(entry.dttm, expected_time)
+
+    @freeze_time("2022-02-10T00:00:00Z")
+    def test_get_latest_entry_exact(self):
+        gpx_collection = parser.parse_gpx_and_sync_now(
+            self.gpx_file_path
+        )
+                
+        # Test is 00:05, we should get exactly 00:05
+        test_time = dttm_parser.parse("2022-02-10T00:00:05Z").replace(tzinfo=None)
+        entry = gpx_collection.get_latest_entry(test_time)
+        expected_time = dttm_parser.parse("2022-02-10T00:00:05Z").replace(tzinfo=None)
+        
+        self.assertEquals(entry.dttm, expected_time)
+
+    @freeze_time("2022-02-10T00:00:00Z")
+    def test_get_latest_entry_non_exact(self):
+        gpx_collection = parser.parse_gpx_and_sync_now(
+            self.gpx_file_path
+        )
+        
+        # Test is 00:03.5, we should get 00:03, not 00:04 or anything
+        test_time = dttm_parser.parse("2022-02-10T00:00:03.5Z").replace(tzinfo=None)
+        entry = gpx_collection.get_latest_entry(test_time)
+        expected_time = dttm_parser.parse("2022-02-10T00:00:03Z").replace(tzinfo=None)
+        
+        self.assertEquals(entry.dttm, expected_time)
+
+    @freeze_time("2022-02-10T00:00:00Z")
+    def test_get_latest_entry_does_not_exist(self):
+        gpx_collection = parser.parse_gpx_and_sync_now(
+            self.gpx_file_path
+        )
+        
+        # Date is too early for any entry
+        test_time = dttm_parser.parse("2022-02-09T00:00:05Z").replace(tzinfo=None)
+        
+        self.assertRaises(
+            GPXException, gpx_collection.get_latest_entry, test_time
+        )
