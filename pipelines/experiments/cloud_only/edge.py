a657569831408bbcea650e86812c6f236a85fa4a
clsandoval <clsandoval@up.edu.ph>
2022-03-14 12:01:47 +0800

    add edge testing for diff fps rates

--- pipelines/experiments/cloud_only/edge.py
+++ pipelines/experiments/cloud_only/edge.py
@@ -2,15 +2,16 @@ from edgenet.client import EdgeNetClient, EdgeNetMessage
 from gpx import uses_gpx
 from config import *
 from .functions import *
-
-# Initialize client
-client = EdgeNetClient(f"ws://{SERVER_HOSTNAME}:{SERVER_PORT}")
-
-# Use decorator
-edge_video_stream = client.uses_sender(start_streaming)
-
-# Register functions
-client.register_function(EDGE_FUNCTION_NAME, edge_video_stream)
-
-# Run client
-client.run()
+for i in range(1,30,1):
+    # Initialize client
+    client = EdgeNetClient(f"ws://{SERVER_HOSTNAME}:{SERVER_PORT}")
+    
+    # Use decorator
+    edge_video_stream = client.uses_sender(start_streaming)
+    
+    # Register functions
+    client.register_function(EDGE_FUNCTION_NAME, edge_video_stream)
+    
+    # Run client
+    client.run()
+    
