3ee324d13758bbe1aee27a47ccc25b5310e011de
clsandoval <clsandoval@up.edu.ph>
2022-03-14 11:58:36 +0800

    add testing for cloud at different fps

--- pipelines/experiments/edge_only/edge.py
+++ pipelines/experiments/edge_only/edge.py
@@ -3,14 +3,16 @@ from gpx import uses_gpx
 from config import *
 from .functions import *
 
-# Initialize client
-client = EdgeNetClient(f"ws://{SERVER_HOSTNAME}:{SERVER_PORT}")
 
-# Use decorator
-edge_only_video_capture = client.uses_sender(capture_video)
-
-# Register functions
-client.register_function(EDGE_ONLY_FUNCTION_NAME, edge_only_video_capture)
-
-# Run client
-client.run()
+for i in range(1,30,1):
+    # Initialize client
+    client = EdgeNetClient(f"ws://{SERVER_HOSTNAME}:{SERVER_PORT}")
+    
+    # Use decorator
+    edge_only_video_capture = client.uses_sender(capture_video)
+    
+    # Register functions
+    client.register_function(EDGE_ONLY_FUNCTION_NAME, edge_only_video_capture)
+    
+    # Run client
+    client.run()
