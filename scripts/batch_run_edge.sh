41aa47b4d234c7d9ff9283f28f764433faac07c2
Carlos Panganiban <cgpanganiban@up.edu.ph>
2022-05-13 13:12:06 +0000

    Fix bash scripts

--- scripts/batch_run_edge.sh
+++ scripts/batch_run_edge.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+python3 -m pipelines.experiments.cloud_only.edge --port 8890
 python3 -m pipelines.experiments.edge_only.edge --port 8891
 python3 -m pipelines.experiments.hybrid.edge --port 8892
